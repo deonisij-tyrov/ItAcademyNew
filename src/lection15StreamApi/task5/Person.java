30ef243c36c9448f9674837a94605c54cf51a068
denis <deonisij-tyrov@mail.ru>
2018-06-13 12:38:56 +0300

    Merge branch 'master' of https://github.com/deonisij-tyrov/ItAcademyNew
    # Conflicts:
    #	src/forLection14/ReversCod
    #	src/lection14/BinaryFile.java
    #	src/lection14/ListDirectory.java
    #	src/lection14/Main.java

---
+++ src/lection15StreamApi/task5/Person.java
@@ -0,0 +1,12 @@
+package lection15StreamApi.task5;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+}
