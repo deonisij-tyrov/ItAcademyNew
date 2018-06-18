f38c86b9f41d9bbdd632d83c96c9cfbc61d7c593
Artyom <artyom.yavorski@gmail.com>
2018-06-18 19:47:18 +0300

    lection 17

---
+++ src/lection17Threads/Scientist.java
@@ -0,0 +1,18 @@
+package lection17Threads;
+
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@Data
+//public class Scientist implements Runnable {
+//    private Map<RobotComponents, Integer> components;
+//
+//    public Scientist() {
+//        this.components = new HashMap<>();
+//    }
+//
+
+}
